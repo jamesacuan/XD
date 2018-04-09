272ebb5e8f1ecab1ab24c95e9b0c625c7fc15af7
jecsacuan <jec@sacuan.com>
2018-04-09 14:10:38 +0800

    implement tabs (draft) to dashboard

--- index.php
+++ index.php
@@ -37,8 +37,14 @@ else if($action=='already_logged_in'){
 
 echo "</div>";
 ?>
-<div class='clearfix'>
-    <div class="btn-group pull-left">
+<div>
+
+<!-- Nav tabs -->
+<ul class="nav nav-tabs clearfix" role="tablist">
+  <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">View All</a></li>
+  <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Helmet Holder</a></li>
+  <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Ticket Holder</a></li>
+  <div class="btn-group pull-right">
         <button type="button" onclick="location.href='createTicket.php'" class="btn btn-primary">Create a Ticket</button>
         <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <span class="caret"></span>
@@ -51,44 +57,63 @@ echo "</div>";
             <li><a href="createTicket.php?product=THC">Ticket Holder - custom logo</a></li>
         </ul>
     </div>
-    <div class='pull-right'>
-        <button type='button' class='btn btn-default'>Delete</button>
-    </div>
+</ul>
+
+<!-- Tab panes -->
+<div class="tab-content">
+  <div role="tabpanel" class="tab-pane active" id="home">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th class="col-xs-1">Ticket Number</th>
+                <th class="col-xs-5">Note</th>
+                <th class="col-xs-2">Image</th>
+                <th class="col-xs-1">Seller</th>
+                <th class="col-xs-2">Date</th>
+                <th class="col-xs-1">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>llsdasasdasdasdasdasdasdsaasd</td>
+                <td>testing.jpg</td>
+                <td>glenn</td>
+                <td>04/09/2018 1:55PM</td>
+                <td>Pending</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+            </tr>
+        </tbody>
+    </table>
+  
+  </div>
+  <div role="tabpanel" class="tab-pane" id="profile">.2.</div>
+  <div role="tabpanel" class="tab-pane" id="messages">..3</div>
 </div>
-<table class="table table-hover">
-    <thead>
-        <tr>
-            <th class="col-xs-1">Ticket ID</th>
-            <th class="col-xs-3">Customer Name</th>
-            <th class="col-xs-6">Note</th>
-            <th class="col-xs-1">Job Order</th>
-            <th class="col-xs-1">Status</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-        </tr>
-        <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            <td>@mdo</td>
-        </tr>
-        <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-            <td>@mdo</td>
-        </tr>
-    </tbody>
-</table>
+
+</div>
+
+<script>
+$('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+</script>
 <?php
     include 'template-footer.php'
 ?>

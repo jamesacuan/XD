693575547332a2401b43d1bea103a620169b85db
jecsacuan <jec@sacuan.com>
2018-04-16 16:34:57 +0800

    minor changes

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ class Database{
     private $host = "localhost";
     private $db_name = "xd";
     private $username = "root";
-    private $password = "";
+    private $password = "root";
     public  $conn;
  
     public function getConnection(){
---
+++ joborder.php
@@ -0,0 +1,13 @@
+<?php
+include_once "config/core.php";
+
+$page_title="Job Order";
+$require_login=true;
+
+include_once "login_check.php";
+include 'template/header.php';
+?>
+
+<?php
+include 'template/footer.php';
+?>
